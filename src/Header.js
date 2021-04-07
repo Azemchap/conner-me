4efbfa0488807afcb1a15bf06adeee1b9719c348
Azem <38106778+Azemchap@users.noreply.github.com>
2021-04-07 07:31:32 +0100

    Initialize project using Create React App

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+
+            <img className="header_logo" src="./img/connerMe.svg" alt="Header logo"/>
+        </div>
+    )
+}
+
+export default Header
