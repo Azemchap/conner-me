3c35132c3af23f799f5a7014e2fba2ed30bafb0f
azem <azemchap@gmail.com>
2021-05-04 17:23:43 +0100

    last commit of the day!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout.js"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
 
-
-    </div>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
